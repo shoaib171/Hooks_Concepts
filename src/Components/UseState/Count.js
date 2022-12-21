617d5ebf02ab2f5cc51bc1ed2bc94c8064749a48
Shoaib Rasheed <shoaib171@gmail.com>
2022-12-21 10:50:38 -0800

    React-Hooks

--- src/Components/UseState/Count.js
+++ src/Components/UseState/Count.js
@@ -1,23 +1,38 @@
-import React, { useState } from 'react'
+/** @format */
+
+import React, { useState } from "react";
 
 const Count = (props) => {
-    const[Count,SetCount]=useState(0)
-    // const incrementvalue=(e)=>{
-    //    e.preventDefault();
-    //    SetCount(Count+1);
-    // }
-    // const decrementvalue=(e)=>{
-    //     e.preventDefault();
-    //     SetCount(Count-1);
-    //  }
+  const [Count, SetCount] = useState(0);
+  // const incrementvalue=(e)=>{
+  //    e.preventDefault();
+  //    SetCount(Count+1);
+  // }
+  // const decrementvalue=(e)=>{
+  //     e.preventDefault();
+  //     SetCount(Count-1);
+  //This is conditional logic to show just zero values not in Minus
+  // if (Count > 0) {
+  //   SetCount(Count - 1);
+  // } else {
+  //   SetCount(0);
+  // }
+  //  }
   return (
-    <div style={{textAlign:"center",position:"relative",top:"30px"}}>
-    <h2>{props.heading}</h2>
-    <button className='btn'  onClick={()=>SetCount(Count+1)}>+</button>
-    <span>{Count}</span>
-    <button  className='btn' onClick={()=>(Count===0 ? SetCount(0):SetCount( Count-1))}>-</button>
+    <div style={{ textAlign: "center", position: "relative", top: "30px" }}>
+      <h2>{props.heading}</h2>
+      <button className="btn" onClick={() => SetCount(Count + 1)}>
+        +
+      </button>
+      <span>{Count}</span>
+      <button
+        className="btn"
+        onClick={() => (Count === 0 ? SetCount(0) : SetCount(Count - 1))}
+      >
+        -
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Count
+export default Count;
